b9c9a0e56b1b3a952160189bbb50d398381dc14a
jammie <jammietorayno@gmail.com>
2024-10-01 23:25:39 +0800

    final local file

--- ajpp5/config/config.php
+++ ajpp5/config/config.php
@@ -1,8 +1,13 @@
 <?php
 $host = 'localhost';
-$dbname = 'u606518727_rs_webapp_db';
-$username = 'u606518727_ajpp5trial';
-$password = 'AJpp5trial-1';
+$dbname = 'rs_webapp_db';
+$username = 'root';
+$password = '';
+
+// $host = 'localhost';
+// $dbname = 'u606518727_rs_webapp_db';
+// $username = 'u606518727_ajpp5trial';
+// $password = 'AJpp5trial-1';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
